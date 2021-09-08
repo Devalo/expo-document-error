d389d1a7e37a38ec762fa957e7c2d953562ad814
Stephan B Valois <stephan@devalo.no>
2021-09-08 09:19:30 +0200

    init

--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import * as DocumentPicker from 'expo-document-picker'
+import * as FileSystem from 'expo-file-system'
+import { Button, Image } from 'react-native'
 
 export default function App() {
+  const [doc, setDoc] = useState()
+
+  const handlePress = async () => {
+    const doc = await DocumentPicker.getDocumentAsync()
+
+    const info = await FileSystem.getInfoAsync(doc.uri)
+    console.log(doc)
+    console.log('---')
+    console.log(info)
+    setDoc(doc)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title='click' onPress={handlePress} />
+      <Image
+        source={{
+          uri: doc && doc.uri
+        }}
+        style={{
+          width: 50,
+          height: 50
+        }}
+      />
     </View>
   );
 }
